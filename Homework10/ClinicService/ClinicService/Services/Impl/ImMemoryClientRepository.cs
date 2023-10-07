d8c1309a50ec43d15dbf8b68449ba36a986657be
Alexandr <a.a.shutov@bk.ru>
2023-10-07 12:22:32 +0300

    Homework10

---
+++ Homework10/ClinicService/ClinicService/Services/Impl/ImMemoryClientRepository.cs
@@ -0,0 +1,32 @@
+﻿using ClinicService.Models;
+
+namespace ClinicService.Services.Impl
+{
+    public class ImMemoryClientRepository : IPetRepository
+    {
+        public int Create(Pet item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pet> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pet GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Pet item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
